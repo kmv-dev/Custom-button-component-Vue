9d2d62d9d5b43c1f5ed498766274b7c3d28e40f5
kireev <kireevmw@yandex.ru>
2023-01-20 21:46:08 +0700

    add button mode

--- src/components/ui/BaseButton/index.vue
+++ src/components/ui/BaseButton/index.vue
@@ -1,7 +1,16 @@
 <template>
-  <button class="base-btn" :class="btnClass" :type="type">
-    <span v-if="isIcon" class="base-btn__icon" :class="iconClass" />
+  <a v-if="link !== ''" class="base-btn__link" :href="link">
     <slot />
+  </a>
+  <button v-else class="base-btn" :class="btnClass" :type="type">
+    <slot name="icon-left"></slot>
+    <slot v-if="!isIcon" />
+    <span
+      v-if="isIcon && iconClass"
+      class="base-btn__icon"
+      :class="iconClass"
+    ></span>
+    <slot name="icon-right"></slot>
   </button>
 </template>
 
@@ -21,22 +30,35 @@ export default {
       type: Boolean,
       default: false,
     },
+    iconClass: {
+      type: String,
+      default: "",
+    },
+    iconSide: {
+      type: String,
+      default: "",
+    },
     isIcon: {
       type: Boolean,
       default: false,
     },
-    iconClass: {
+    link: {
       type: String,
       default: "",
     },
   },
   computed: {
     btnClass() {
-      const { mode, disabled } = this;
+      const { mode, disabled, isIcon } = this;
       return [
         {
           "base-btn_disabled": disabled,
-          "base-btn_logout": mode === "logout",
+          "base-btn_secondary": mode === "secondary",
+          "base-btn_warning": mode === "warning",
+          "base-btn_info": mode === "info",
+          "base-btn_danger": mode === "danger",
+          "base-btn_action": mode === "action",
+          "base-btn_icon": isIcon,
         },
       ];
     },
@@ -47,28 +69,51 @@ export default {
 <style lang="scss" scoped>
 .base-btn {
   padding: 14px 46px;
-  border-top-left-radius: 34px;
-  border-bottom-left-radius: 38px;
-  border-top-right-radius: 38px;
-  border-bottom-right-radius: 34px;
+  border-top-left-radius: 32px;
+  border-bottom-left-radius: 36px;
+  border-top-right-radius: 32px;
+  border-bottom-right-radius: 26px;
   background: #702c7e;
   color: #ffffff;
-  font-size: 18px;
+  font-size: 16px;
   transition: 0.2s ease-in-out;
   text-transform: uppercase;
-  &:hover {
-    box-shadow: none;
+  &__link {
+    color: #c4296c;
+    transition: 0.2s ease-in-out;
+    &:hover {
+      color: #767679;
+    }
   }
-  &__icon {
-    margin-right: 5px;
+  &_secondary {
+    background: #c4296c;
+  }
+  &_warning {
+    background: #f4ba46;
   }
   &_disabled {
     pointer-events: none;
-    opacity: 0.5;
+    background: #efefef;
+    color: #767679;
   }
-  &_logout {
-    background: #f2f0f2;
-    color: #5787a4;
+  &_info {
+    background: #0083b6;
+  }
+  &_danger {
+    background: #df3f3e;
+  }
+  &_action {
+    background: #ed732e;
+  }
+  &_icon {
+    padding: 14px 30px;
+    max-width: 60px;
+    height: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 24px;
+    border-radius: 100%;
   }
 }
 </style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,7 +1,40 @@
 <template>
   <div class="content">
     <div class="content__inner">
-      <BaseButton>отправить письмо</BaseButton>
+      <BaseButton class="content__btn" :link="'#'"
+        >Напомнить PIN-код</BaseButton
+      >
+      <BaseButton class="content__btn">отправить письмо</BaseButton>
+      <BaseButton
+        class="content__btn"
+        :mode="'secondary'"
+        :iconClass="'icon-quest'"
+        :isIcon="true"
+      ></BaseButton>
+      <BaseButton
+        class="content__btn"
+        :mode="'warning'"
+        :iconClass="'icon-arrow'"
+        ><template v-slot:icon-left
+          ><span class="icon__left icon-arrow"></span></template
+        ><template v-slot:icon-right
+          ><span class="icon__right icon-exit"></span></template
+        >отправить письмо</BaseButton
+      >
+      <BaseButton class="content__btn" :mode="'info'"
+        ><template v-slot:icon-left
+          ><span class="icon__left icon-arrow"></span></template
+        >отправить письмо</BaseButton
+      >
+      <BaseButton class="content__btn" :mode="'danger'"
+        ><template v-slot:icon-right
+          ><span class="icon__right icon-arrow-right"></span></template
+        >отправить письмо</BaseButton
+      >
+      <BaseButton class="content__btn" :mode="'action'"
+        >отправить письмо</BaseButton
+      >
+      <BaseButton class="content__btn" disabled>отправить письмо</BaseButton>
     </div>
   </div>
 </template>
@@ -14,9 +47,24 @@
     background-color: aquamarine;
   }
   &__inner {
+    display: grid;
+    grid-template-columns: max-content;
+    justify-content: center;
+    justify-items: center;
     margin: 0 auto;
     padding-top: 50px;
     text-align: center;
+  }
+  &__btn {
+    margin-bottom: 10px;
+  }
+}
+.icon {
+  &__left {
+    margin-right: 10px;
+  }
+  &__right {
+    margin-left: 10px;
   }
 }
 </style>
